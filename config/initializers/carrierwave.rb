6a2c5c1f20be853ce59f935e7ebdee1f5d2fa4e9
David Táboas <david.taboas.glez@gmail.com>
2014-11-29 19:24:00 +0100

    Archivos en S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,4 +5,15 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
+
+
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage           = :file
+    config.enable_processing = false
+    config.root              = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
+
 end
